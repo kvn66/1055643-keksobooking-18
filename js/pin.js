a847fe52175612a6fb355ab6b20374eb1edbf602
Vadim Kalashnikov <vadim.kalashnikov66@gmail.com>
2019-09-30 13:01:29 +0500

    Начал выполнять задание

---
+++ js/pin.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var createPin = function (dataElement) {
+  var similarPinTemplate = document.querySelector('#pin')
+    .content
+    .querySelector('.map__pin');
+  var pinElement = similarPinTemplate.cloneNode(true);
+
+  pinElement.style = 'left: ' + dataElement.location.x + 'px; top: ' + dataElement.location.y + 'px;';
+  pinElement.querySelector('img').src = dataElement.author.avatar;
+  pinElement.querySelector('img').alt = dataElement.offer.title;
+
+  return pinElement;
+};
+
+var createPins = function (dataArray) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < dataArray.length; i++) {
+    fragment.appendChild(createPin(dataArray[i]));
+  }
+  return fragment;
+};
+
+(function () {
+  window.pins = createPins();
+})();
